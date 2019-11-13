688507c48b32538b490a552ba78724f2a29114a8
valentinlauret <34425103+valentinlauret@users.noreply.github.com>
2019-11-13 14:23:36 +0100

    Update and rename Single-visit_Occupancy.md to Aerial.R

---
+++ Aerial.R
@@ -0,0 +1,97 @@
+#############################
+## Aerial SV modelling  
+#############################
+
+ 
+############################# 
+# Specify model in BUGS language
+#############################
+
+library(R2jags)
+
+sink("Aerial_SV.jags")
+cat("
+    model {
+
+    # priors
+
+    alpha.psi ~ dnorm(0,0.444) # occupancy intercept
+    alpha.p ~ dnorm(0,0.444) # detection intercept 
+    beta.sst ~ dnorm(0,0.444) # slope sst effect
+    beta.bathy ~ dnorm(0,0.444)	# slope bathy effect
+    beta.eff ~ dnorm(0,0.444)	# slope survey effort effect
+    
+    for (i in 1:nsite){
+
+      z[i] ~ dbern(psi[i])
+
+      logit(psi[i]) <- lpsi[i]
+
+      lpsi[i] <- alpha.psi + beta.sst * SST[i] + beta.bathy * BATHY[i]
+      
+      mu.p[i] <- z[i] * p[i] 
+    
+      logit(p[i]) <- lp[i] 
+      
+      lp[i] <- alpha.p + beta.eff * EFF[i]
+      
+      y[i] ~ dbern(mu.p[i])
+   
+     } #i
+    
+  }
+    ", fill = TRUE)
+sink()
+
+#############################
+# Data & run
+#############################
+
+load("Aerial_sv.rdata")
+
+# see loaded data 
+
+str(win.data)
+
+# see initial values 
+
+inits
+
+# see saved parameters
+
+params
+
+# MCMC settings
+
+ni <- 300000
+nt <- 5
+nb <- 150000
+nc <- 3
+
+# Call JAGS from R
+
+out <- jags(win.data, params, "Aerial_SV.jags", inits = inits, n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, working.directory = getwd())
+
+Aerial_sv <- out
+
+save(Aerial_sv, file="RES_Aerial_sv.rdata")
+
+#############################
+# Outputs
+#############################
+
+library(mcmcplots)
+
+load("RES_Aerial_sv.rdata")
+
+head(out$BUGSoutput$summary)
+
+# see posterior distribution of parameters
+
+denplot(out, c("alpha.psi","beta.bathy", "beta.sst"))
+
+# see parameters values for saved iterations
+
+traplot(out,"beta.bathy")
+traplot(out,"beta.sst")
+traplot(out,"beta.eff")
